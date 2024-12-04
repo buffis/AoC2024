b68e3c83b74addec065f30dab662bbb14470b144
Björn Kempen <bjorn.kempen@gmail.com>
2024-12-04 21:42:03 +0100

    day2

---
+++ 2/2.py
@@ -0,0 +1,27 @@
+from itertools import pairwise
+
+def is_safe(x):
+  y = [y[0] - y[1] for y in pairwise(x)]
+  return not [x for x in y if x<1 or x>3] \
+      or not [x for x in y if x>-1 or x<-3]
+
+def get_number_rows(filename):
+  return [list(map(int, line.split())) for line in open(filename).readlines()]
+
+# Part 1:
+r = 0
+for x in get_number_rows("in.txt"):
+  if is_safe(x):
+    r += 1
+print (r)
+
+# Part 2:
+r = 0
+for x in get_number_rows("in.txt"):
+  for i in range(len(x)):
+    y = x[:]
+    del(y[i])
+    if is_safe(y):
+      r += 1
+      break
+print (r)
